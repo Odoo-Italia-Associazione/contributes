ee8c979e957e04afe3f9e93127067df164ee65f8
SGiuseppe <gstoduto@gmail.com>
2017-11-12 19:15:30 +0100

    First version of the Web_Hide_Column module
    Simplifies column management in a list_views

---
+++ web_hide_column/README.rst
@@ -0,0 +1,57 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+==========================
+Nasconde/Mostra colonne
+==========================
+
+Mostra o nasconde le colonne in una list_views
+
+Usage
+=====
+
+In una Tree views si può assegnare il widget in questo modo:
+
+<field name="withholding_tax_exclude" widget="hide_column" check_field="test_withholding_tax"/>
+
+dove:
+
+- hide_column - è il nome di questo widget
+- check_field - è il checkbox di controllo per mostrare(true) o nascondere(false) la colonna in oggetto. In questo caso withholding_tax_exclude
+
+
+
+Bug Tracker
+===========
+
+Eventuali Bug cominicarli ai Contributors
+
+Credits
+=======
+
+Contributors
+------------
+
+* Giuseppe Stoduto
+* Odoo Italia Associazione
+
+Maintainer
+----------
+
+-  Giuseppe Stoduto
+-  https://github.com/SGiuseppe
+
+Questo modulo è mantenuto da Giuseppe Stoduto per eventuali bug e modifiche 
+contattare il Maiteiner.
+
+.. image:: https://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: https://odoo-community.org
+
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
+
+To contribute to this module, please visit https://odoo-community.org.
---
+++ web_hide_column/static/src/js/hide_column.js
@@ -0,0 +1,112 @@
+//-*- coding: utf-8 -*-
+//############################################################################
+//
+//   OpenERP, Open Source Management Solution
+//   This module copyright
+//     (C) 2013 Therp BV (<http://therp.nl>).
+//     (c) 2015 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
+//              Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU Affero General Public License as
+//   published by the Free Software Foundation, either version 3 of the
+//   License, or (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU Affero General Public License for more details.
+//
+//   You should have received a copy of the GNU Affero General Public License
+//   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//############################################################################
+// "use strict";
+openerp.molino_withholding = function(instance, local) {
+    var _t = instance.web._t,
+        _lt = instance.web._lt;
+    var QWeb = instance.web.qweb;
+
+    instance.web.form.FieldOne2Many.include({
+
+        start: function() {
+
+            // this._super.apply(this, arguments);
+
+            var self = this;
+
+            var destroy = function() {
+                self.is_loaded = self.is_loaded.then(function() {
+                    self.viewmanager.destroy();
+                    return $.when(self.load_views()).done(function() {
+                        self.reload_current_view();
+                    });
+                });
+            };
+
+            this.is_loaded.done(function() {
+                if (typeof self.field.views.tree !== 'undefined') {
+                    _fields = self.field.views.tree.arch.children;
+                    // Cerca se esiste il widget hide_column
+                    _.each(_fields, function(el) {
+                        if (el.attrs.widget == 'hide_column') {
+                            var _checkbox = $(":checkbox[name='" + el.attrs.check_field + "']");
+                            // Soluzione valida ma non mostra la colonna se il campo viene modificato
+                            // da un evento esterno.
+                            // _checkbox.on("change", self, destroy);
+                            //
+                            // Molto meglio del metodo sopra indicato.
+                            self.field_manager.on("field_changed:" + _checkbox[0].name, self, destroy);
+                        }
+                    });
+                }
+            });
+            // return this._super(this, arguments);
+            // non da problemi anche così.
+            return this._super();
+        },
+    });
+
+
+    instance.web.ListView.include({
+
+        setup_columns: function (fields, grouped) {
+
+            var registry = instance.web.list.columns;
+            this.columns.splice(0, this.columns.length);
+            this.columns.push.apply(this.columns,
+                _(this.fields_view.arch.children).map(function (field) {
+                    var id = field.attrs.name;
+                    return registry.for_(id, fields[id], field);
+            }));
+            if (grouped) {
+                this.columns.unshift(
+                    new instance.web.list.MetaColumn('_group', _t("Group")));
+            }
+
+            this.visible_columns = this.effective_column(this.columns);
+            this.aggregate_columns = _(this.visible_columns).invoke('to_aggregate');
+        },
+
+        effective_column: function(columns)
+        {
+           result = _.filter(columns, function (column) {
+                    if (column.widget == 'hide_column' && column.check_field !== '') {
+                        var $checkbox = $(":checkbox[name='" + column.check_field + "']");
+                        if ($checkbox.prop('checked')){
+                            column.invisible = '0';
+                            column.modifiers.invisible = false;
+                            return true;
+                        } else{
+                            column.invisible = '1';
+                            column.modifiers.invisible = true;
+                            return false;
+                        }
+                    } else {
+                        return column.invisible !== '1';
+                    }
+                });
+           return result;
+        },
+    });
+};
